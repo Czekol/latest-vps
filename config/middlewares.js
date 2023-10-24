7ac9b772ebc6647e80604792f4e9294b8e995178
Bartosz <121594682+Czekol@users.noreply.github.com>
2023-10-24 21:44:23 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,25 @@
-// module.exports = [
-//   'strapi::errors',
-//   'strapi::security',
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::logger',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
 
- module.exports = ({ env }) => ({
-      settings: {
-        cors: {
-          enabled: true,
-          // configure CORS to app;s client side and Strapi client (admin panel)
-          // client-side 1: http://localhost:3000
-          // client-side 2: http://localhost:8000
-          // Strapi client (admin panel): http://localhost:1337
-          origin: ['https://genialnastrona.pl/', 'https://api.genialnastrona.pl/'], // Add your allowed domains in the array, e.g., ['http://localhost:3000', 'http://yourdomain.com']
-        },
-      },
-    });
+ // module.exports = ({ env }) => ({
+ //      settings: {
+ //        cors: {
+ //          enabled: true,
+ //          // configure CORS to app;s client side and Strapi client (admin panel)
+ //          // client-side 1: http://localhost:3000
+ //          // client-side 2: http://localhost:8000
+ //          // Strapi client (admin panel): http://localhost:1337
+ //          origin: ['https://genialnastrona.pl/', 'https://api.genialnastrona.pl/'], // Add your allowed domains in the array, e.g., ['http://localhost:3000', 'http://yourdomain.com']
+ //        },
+ //      },
+ //    });
