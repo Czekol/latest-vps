54ad228904f84988ffb46f9a3992a1d96292483e
Bartosz <121594682+Czekol@users.noreply.github.com>
2023-10-24 22:51:35 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,21 +1,63 @@
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      enabled: true,
-      origin: [process.env.CORS_ORIGIN], 
+module.exports = [
+  {
+    name: 'strapi::errors',
+    config: {
+      // Konfiguracja błędów
     },
   },
-  default: [
-    'strapi::errors',
-    'strapi::security',
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::logger',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
-  ],
-});
-
+  {
+    name: 'strapi::security',
+    config: {
+      // Konfiguracja zabezpieczeń
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: process.env.CORS_ORIGIN, // Pobieranie dozwolonych domen z zmiennych środowiskowych
+    },
+  },
+  {
+    name: 'strapi::poweredBy',
+    config: {
+      // Konfiguracja wyświetlania "Powered by Strapi"
+    },
+  },
+  {
+    name: 'strapi::logger',
+    config: {
+      // Konfiguracja loggera
+    },
+  },
+  {
+    name: 'strapi::query',
+    config: {
+      // Konfiguracja zapytań
+    },
+  },
+  {
+    name: 'strapi::body',
+    config: {
+      // Konfiguracja przetwarzania ciała zapytania
+    },
+  },
+  {
+    name: 'strapi::session',
+    config: {
+      // Konfiguracja sesji
+    },
+  },
+  {
+    name: 'strapi::favicon',
+    config: {
+      // Konfiguracja favicon
+    },
+  },
+  {
+    name: 'strapi::public',
+    config: {
+      // Konfiguracja plików publicznych
+    },
+  },
+  // Dodaj inne wpisy middleware w miarę potrzeb
+];
